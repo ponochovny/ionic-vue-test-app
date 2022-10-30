a347cf4ab86dbbd0221d26b5dac5b538f879a52f
ponochovny <ponochovny@gmail.com>
2022-10-30 16:41:19 +0200

    memory details init

---
+++ src/pages/MemoryDetails.vue
@@ -0,0 +1,15 @@
+<template>
+	<base-layout page-title="The Details" page-default-back-link="/memories">
+		<h2>The details page</h2>
+	</base-layout>
+</template>
+
+<script>
+import {} from '@ionic/vue'
+
+export default {
+	components: {},
+}
+</script>
+
+<style scoped></style>
