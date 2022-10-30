84c4b0df29fe4db350fa489855565ac250096bb8
ponochovny <ponochovny@gmail.com>
2022-10-30 16:40:44 +0200

    back button

--- src/components/base/BaseLayout.vue
+++ src/components/base/BaseLayout.vue
@@ -2,6 +2,11 @@
 	<ion-page>
 		<ion-header>
 			<ion-toolbar>
+				<ion-buttons slot="start">
+					<ion-back-button
+						:default-href="pageDefaultBackLink"
+					></ion-back-button>
+				</ion-buttons>
 				<ion-title>{{ pageTitle }}</ion-title>
 			</ion-toolbar>
 		</ion-header>
@@ -18,16 +23,20 @@ import {
 	IonToolbar,
 	IonTitle,
 	IonContent,
+	IonBackButton,
+	IonButtons,
 } from '@ionic/vue'
 
 export default {
-	props: ['pageTitle'],
+	props: ['pageTitle', 'pageDefaultBackLink'],
 	components: {
 		IonPage,
 		IonHeader,
 		IonToolbar,
 		IonTitle,
 		IonContent,
+		IonBackButton,
+		IonButtons,
 	},
 }
 </script>
