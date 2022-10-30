85021906b0175206abd3ec9304c3daea481203c1
ponochovny <ponochovny@gmail.com>
2022-10-30 19:01:33 +0200

    renamed memory details page and added Memory Overview component

--- src/pages/MemoryDetailsPage.vue
+++ src/pages/MemoryDetailsPage.vue
@@ -4,15 +4,22 @@
 		page-default-back-link="/memories"
 	>
 		<h2 v-if="!loadedMemory">Could not find a memory for the given id.</h2>
-		<h2 v-else>Loaded it</h2>
+		<memory-overview
+			v-else
+			:title="loadedMemory.title"
+			:image="loadedMemory.image"
+			:description="loadedMemory.description"
+		></memory-overview>
 	</base-layout>
 </template>
 
 <script>
-import {} from '@ionic/vue'
+import MemoryOverview from '../components/memories/MemoryOverview.vue'
 
 export default {
-	components: {},
+	components: {
+		MemoryOverview,
+	},
 	data() {
 		return {
 			memoryId: this.$route.params.id,
