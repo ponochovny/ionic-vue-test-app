c5c8a7cecdf72fc6df8df1707948610a5d129560
ponochovny <ponochovny@gmail.com>
2022-10-30 18:51:12 +0200

    decomposed with components

---
+++ src/components/memories/MemoriesList.vue
@@ -0,0 +1,22 @@
+<template>
+	<ion-list>
+		<memory-item
+			v-for="memory of memories"
+			:key="memory.id"
+			:memory="memory"
+		></memory-item>
+	</ion-list>
+</template>
+
+<script>
+import { IonList } from '@ionic/vue'
+import MemoryItem from './MemoryListItem.vue'
+
+export default {
+	props: ['memories'],
+	components: {
+		IonList,
+		MemoryItem,
+	},
+}
+</script>
