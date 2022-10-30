cf422e54f7bdb34a738ff190fcb3e6cb6fa4c429
ponochovny <ponochovny@gmail.com>
2022-10-30 18:40:47 +0200

    getting data from store

--- src/pages/MemoryDetails.vue
+++ src/pages/MemoryDetails.vue
@@ -1,6 +1,10 @@
 <template>
-	<base-layout page-title="The Details" page-default-back-link="/memories">
-		<h2>The details page</h2>
+	<base-layout
+		:page-title="loadedMemory ? loadedMemory.title : 'Loading...'"
+		page-default-back-link="/memories"
+	>
+		<h2 v-if="!loadedMemory">Could not find a memory for the given id.</h2>
+		<h2 v-else>Loaded it</h2>
 	</base-layout>
 </template>
 
@@ -9,6 +13,21 @@ import {} from '@ionic/vue'
 
 export default {
 	components: {},
+	data() {
+		return {
+			memoryId: this.$route.params.id,
+		}
+	},
+	computed: {
+		loadedMemory() {
+			return this.$store.getters.memory(this.memoryId)
+		},
+	},
+	// watch: {
+	// 	$route(currentRoute) {
+	// 		this.memoryId = currentRoute.params.id
+	// 	},
+	// },
 }
 </script>
 
