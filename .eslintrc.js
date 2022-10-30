189d172bdb17ba4307e53d2eb1c9c1b341b3696a
ponochovny <ponochovny@gmail.com>
2022-10-30 16:02:22 +0200

    eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,8 @@ module.exports = {
 		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
 		'vue/no-deprecated-slot-attribute': 'off',
+		'vue/no-unused-components': 0,
+		'no-unused-vars': 0,
 	},
 	overrides: [
 		{
